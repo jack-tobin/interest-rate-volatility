9d34f0d0ac2b56402e332844bf0990d95943e70a
Jack Tobin <jtobin@Jacks-MacBook-Pro.local>
2021-05-28 19:31:34 -0400

    began implementing garch model, working on visualizations. still need to clean up code overall and fine tune parameters

--- rate_volatility.py
+++ rate_volatility.py
@@ -7,15 +7,14 @@ Created on Fri May 28 09:11:14 2021
 """
 
 import os
-import git
 import pandas as pd
 import numpy as np
 import datetime as dt
 import matplotlib.pyplot as plt
 import matplotlib.ticker as mtick
 from mpl_toolkits.mplot3d import Axes3D
-from mpl_toolkits.mplot3d import proj3d
-from matplotlib import cm
+from scipy import stats
+from arch import arch_model
 import seaborn as sns
 plt.style.use('seaborn-deep')
 import quandl
@@ -26,15 +25,13 @@ from statsmodels.graphics.tsaplots import plot_acf
 local_dir = os.path.expanduser('~/Documents/projects/rate_volatility')
 os.chdir(local_dir)
 
-# location of remote git repository
-repo_url = 'https://github.com/james-j-tobin/interest-rate-volatility.git'
+# load in authenticate quandl code
+from auth_quandl import authenticate_quandl
+quandl = authenticate_quandl(quandl)
 
 # load in rates data, scrub
 def load_rates_data(refresh=False):
     if refresh:
-        # authenticate quandl api
-        quandl.ApiConfig.api_key = 'fExJA3CeSuzM_fBVZkiJ'
-        
         # load in data
         rates = quandl.get('USTREASURY/YIELD')
         
@@ -77,7 +74,7 @@ def plot_latest_curve():
     plt.gcf().savefig('latest_curve.png', dpi=600)
     plt.clf()
     
-# plot_latest_curve()
+plot_latest_curve()
 
 # surface plot of curve over time
 
@@ -114,42 +111,51 @@ def yc_surface():
     plt.gcf().savefig('yc_surface.png', dpi=600)
     plt.clf()
     
-# yc_surface()
+yc_surface()
 
-# 1Y volatility analysis
+# 10YT volatility analysis
 
-# subset 10Y
-t10 = pd.DataFrame(rates[label_vals['10 YR']].copy())
-t10.columns = ['10Y']
-
-# additional data: changes in rates, decomposed daily return
-t10['Diff'] = t10['10Y'].diff()
-t10['LogDiff']= np.log(t10['10Y']).diff().mul(100)
-t10['Ret'] = t10['10Y'].apply(lambda x: ((1 + x)**(1/360) - 1) * 100)
-t10['Vol'] = t10['Diff'].rolling(60).std()
-
-# plot
-fig, axes = plt.subplots(3, figsize=(10,10))
-
-# first plot--yield over time
-axes[0].plot(t10['10Y'], label='10Y Yield')
-
-# second plot--changes in yield
-axes[1].plot(t10['Diff'], label='Change in 10Y Yield')
-
-# third plot--rolling volatility of changes in yield
-axes[2].plot(t10['Vol'], label='Rolling 30D Rate Vol')
-
-# clean up
-axes[0].yaxis.set_major_formatter(mtick.PercentFormatter())
-axes[1].yaxis.set_major_formatter(mtick.PercentFormatter())
-axes[2].yaxis.set_major_formatter(mtick.PercentFormatter())
-fig.subplots_adjust(bottom=0.05, top=0.95, left=0.05, right=0.95)
-[ax.legend() for ax in axes]
-
-# save
-plt.gcf().savefig('10y_stats.png', dpi=600)
-plt.clf()
+def vol_analysis(colname):
+    # subset 10Y
+    df = pd.DataFrame(rates[label_vals[colname]].copy())
+    df.columns = [colname]
+    
+    # additional data: changes in rates, decomposed daily return
+    df['Diff'] = df[colname].diff()
+    df['LogDiff']= np.log(df[colname]).diff().mul(100)
+    df['Ret'] = df[colname].apply(lambda x: ((1 + x)**(1/360) - 1) * 100)
+    df['Vol'] = df['Diff'].rolling(90).std()
+    
+    # plot
+    fig, axes = plt.subplots(3, figsize=(10,10))
+    
+    # first plot--yield over time
+    axes[0].plot(df[colname], label=colname + ' Yield')
+    
+    # second plot--changes in yield
+    axes[1].plot(df['Diff'], label='Change in ' + colname + ' Yield')
+    
+    # third plot--rolling volatility of changes in yield
+    axes[2].plot(df['Vol'], label='Rolling 30D Rate Vol')
+    
+    # clean up
+    axes[0].yaxis.set_major_formatter(mtick.PercentFormatter())
+    axes[1].yaxis.set_major_formatter(mtick.PercentFormatter())
+    axes[2].yaxis.set_major_formatter(mtick.PercentFormatter())
+    fig.subplots_adjust(bottom=0.05, top=0.95, left=0.05, right=0.95)
+    [ax.legend() for ax in axes]
+    
+    # save
+    plt.gcf().savefig(colname + '_stats.png', dpi=600)
+    plt.clf()
+    
+    return df
+    
+# run vol analyses
+t3m = vol_analysis('3 MO')
+t2 = vol_analysis('2 YR')
+t10 = vol_analysis('10 YR')
+t30 = vol_analysis('30 YR')
 
 # Hurst Exponent function
 # credit: https://goldinlocks.github.io/ARCH_GARCH-Volatility-Forecasting/
@@ -172,7 +178,7 @@ def hurst(S):
     return poly[0] * 2.0
 
 # run hurst coefficient
-hurst(t10['10Y'].values)
+hurst(t10['10 YR'].values)
 hurst(t10['Diff'].dropna().values)
 hurst(t10['LogDiff'].dropna().values)
 
@@ -199,6 +205,86 @@ subset = t10['LogDiff'].dropna()
 standardized = subset.sub(subset.mean()).pow(2)
 corr_plot(subset, 'Log Diff Autocorr')
 corr_plot(standardized, 'Standardized Log Diff Autocorr')
+
+# apply garch model to the time series to forecast volatility
+
+# fit model
+X = t10['Diff'].dropna()
+gm = arch_model(X, p=1, o=1, q=1, mean='zero', vol='GARCH', dist='skewt')
+result = gm.fit(update_freq=0, disp='off')
+result.plot()
+result.summary()
+
+# plot distribution of residuals
+
+# calculate standardized residuals
+gm_resid = result.resid
+gm_std = result.conditional_volatility
+gm_resid_st = gm_resid / gm_std
+
+# create plot
+fig, ax = plt.subplots(1, figsize=(10,7))
+sns.distplot(gm_resid_st, norm_hist=True, fit=stats.norm, bins=50, ax=ax)
+
+# evaluate fit by overlaying conditional vol over daily diffs
+fig, ax = plt.subplots(1)
+ax.plot(X, color='gray', label='Daily Yield Change', alpha=0.4)
+ax.plot(gm_std, color='darkred', label='Conditional Volatility')
+ax.legend()
+
+# plot conditional variance against rolling 30d variance
+X_roll_vol = X.rolling(30).std()
+fig, ax = plt.subplots(1)
+ax.plot(gm_std, color='darkred', label='Conditional Volatility')
+ax.plot(X_roll_vol, color='darkgreen', label='Actual Rolling 30D Volatility')
+ax.legend()
+
+# simulate forward returns
+
+clrs = sns.color_palette('deep')
+n_periods = 30
+
+def forecasting(n_days):
+    
+    n_days = 30
+
+    # forecast
+    forecast = result.forecast(start=0, horizon=n_days, reindex=False,
+                               method='simulation')
+    paths = forecast.simulations.residual_variances[-1].T
+    
+    # generate plots
+    fig, ax = plt.subplots(1)
+    ax.plot(paths * 100, alpha=0.10, color=clrs[0])
+    ax.yaxis.set_major_formatter(mtick.PercentFormatter())
+    fig.suptitle('Simulated 10YT Volatility Paths')
+    ax.set_xlabel('Forecast Volatility Period (Days)')
+    ax.set_ylabel('Forecast Volatility (%)')
+    plt.gcf().savefig('simulation.png', dpi=600)
+    plt.clf()
+    
+    # hedgehog plot
+    y = np.sqrt(forecast.residual_variance.dropna())['2017-12-31':] * 100
+    vol = result.conditional_volatility['2017-12-31':] * 100
+    
+    fig, ax = plt.subplots(1, figsize=(9,6))
+    ax.plot(vol, color=clrs[0], alpha=0.5, label='Estimated Conditional Volatility')
+    for i in range(0, len(vol) - n_days, n_days):
+        f = y.iloc[i]
+        loc = vol.index.get_loc(f.name)
+        f.index = vol.index[(loc+1):(loc+n_days+1)]
+        if i == 0:
+            ax.plot(f, color=clrs[1], label='Forecast Volatility Path')
+        else:
+            ax.plot(f, color=clrs[1])
+    
+    # clean up
+    ax.legend(loc='upper left')
+    fig.suptitle('Estimated Conditional Volatility vs. 30D Forecast Volatility Paths')
+    ax.yaxis.set_major_formatter(mtick.PercentFormatter())
+    
+    
+
 
 
 
